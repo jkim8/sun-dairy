c958c87b4c04599d3b3bedb01a6f3c814b79d838
jkim8 <juno0218@naver.com>
2022-09-01 17:08:42 -0400

    Write Screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./navigator";
 
 export default function App() {
