c807b5a885d362d5c0764e445d174daa85b8fdd1
jkim8 <juno0218@naver.com>
2022-09-01 15:17:45 -0400

    home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,39 @@
 import react from "react";
 import styled from "styled-components/native";
+import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const View = styled.View`
   flex: 1;
+  padding: 0px 50px;
+  padding-top: 100px;
+  background-color: ${colors.bgColor};
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 38px;
+  margin-bottom: 100px;
+`;
+const Btn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
   justify-content: center;
   align-items: center;
+  background-color: ${colors.btnColor};
+  elevation: 5;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `;
-const Text = styled.Text``;
-const Btn = styled.TouchableOpacity``;
-const BtnText = styled.Text``;
 
 const Home = ({ navigation: { navigate } }) => (
   <View>
+    <Title>Sun Diary</Title>
     <Btn onPress={() => navigate("Write")}>
-      <BtnText>Go Write</BtnText>
+      <Ionicons name="add" color="white" size={40} />
     </Btn>
-    <Text>Home</Text>
   </View>
 );
 
