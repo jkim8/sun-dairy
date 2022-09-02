ce2e059817eee90f1a8d4b9e27cee3c117b2536c
jkim8 <juno0218@naver.com>
2022-09-02 14:53:43 -0400

    reading objects

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
-import react, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
-import colors from "../colors";
 import { Ionicons } from "@expo/vector-icons";
+import colors from "../colors";
 import { useDB } from "../context";
+import { FlatList } from "react-native";
 
 const View = styled.View`
   flex: 1;
@@ -13,6 +14,7 @@ const View = styled.View`
 const Title = styled.Text`
   color: ${colors.textColor};
   font-size: 38px;
+  font-weight: 500;
   margin-bottom: 100px;
 `;
 const Btn = styled.TouchableOpacity`
@@ -21,30 +23,66 @@ const Btn = styled.TouchableOpacity`
   right: 50px;
   height: 80px;
   width: 80px;
-  border-radius: 50;
+  border-radius: 40px;
   justify-content: center;
   align-items: center;
   background-color: ${colors.btnColor};
   elevation: 5;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+`;
+
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const Emotion = styled.Text`
+  font-size: 24px;
+  margin-right: 10px;
+`;
+const Message = styled.Text`
+  font-size: 18px;
+`;
+const Separator = styled.View`
+  height: 10px;
 `;
 
 const Home = ({ navigation: { navigate } }) => {
-  const realm = useDB(
-    useEffect(() => {
+  const realm = useDB();
+  const [feelings, setFeelings] = useState([]);
+  useEffect(() => {
+    const feelings = realm.objects("Feeling");
+    setFeelings(feelings);
+    feelings.addListener(() => {
       const feelings = realm.objects("Feeling");
-      console.log(feelings);
-    }, [])
-  );
-
+      setFeelings(feelings);
+    });
+    return () => {
+      feelings.removeAllListeners();
+    };
+  }, []);
   return (
     <View>
-      <Title>Sun Diary</Title>
+      <Title>My journal</Title>
+      <FlatList
+        data={feelings}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(feeling) => feeling._id + ""}
+        renderItem={({ item }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
       <Btn onPress={() => navigate("Write")}>
         <Ionicons name="add" color="white" size={40} />
       </Btn>
     </View>
   );
 };
-
 export default Home;
